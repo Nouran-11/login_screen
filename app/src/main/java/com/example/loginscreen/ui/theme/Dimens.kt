62f2c5f1199ab082c9003c9306cd3025ade171f4
Nouran Atef <engineer_nouran@yahoo.com>
2025-07-07 06:29:34 +0200

    Add strings, dimensions, and colors for login screen

---
+++ app/src/main/java/com/example/loginscreen/ui/theme/Dimens.kt
@@ -0,0 +1,29 @@
+package com.example.loginscreen.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+object Dimens {
+    val PaddingSmall = 8.dp
+    val PaddingMedium = 16.dp
+    val PaddingLarge = 40.dp
+    val PaddingExtraLarge = 50.dp
+    val PaddingLoginTitle = 140.dp
+
+    val SpacerSmall = 10.dp
+    val SpacerMedium = 16.dp
+    val SpacerLarge = 24.dp
+    val SpacerExtraLarge = 30.dp
+
+
+    val IconSizeSmall = 20.dp
+    val IconSizeMedium = 30.dp
+    val IconSizeLarge = 40.dp
+
+    val ButtonHeight = 56.dp
+    val CornerRadius = 20.dp
+
+    val TitleFontSize = 45.sp
+    val ButtonFontSize = 30.sp
+    val BodyFontSize = 15.sp
+}
